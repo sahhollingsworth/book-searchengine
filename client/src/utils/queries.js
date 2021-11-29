f5faf0530b7aad0f33cd9d0cff4893bd2ab69a35
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-29 00:50:00 -0800

    Added queries util to support retrival of user documents by client components

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+// queries have to be contained in a gql function in order to execute
+import { gql } from '@apollo/client';
+
+
+// Wrap query in the gql function and export for use in our component 
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            password
+            savedBooks
+            bookCount
+        }
+    }
+`;
