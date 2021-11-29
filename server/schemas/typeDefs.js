78eea742f3db2e1c75d7f086bd6d07eb7f38d4a3
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-28 20:56:25 -0800

    Defined entry point to access data (User).  Defined User, Book, and Auth objects for querying

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,35 @@
 const { gql } = require('apollo-server-express');
 
 // Define:
-// book
-// user
-// auth
-// query
 // mutation
 
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        savedBooks: [Book]!
+        bookCount: Int
+    }
+
+    type Book {
+        bookID: String!
+        authors: [String]!
+        title: String!
+        description: String
+        image: String!
+        link: String!
+    }
+
+    type Auth {
+        token: ID!
+        User: User
+    }
+
+    type Query {
+        me: User
+    }
 `;
+//Books.authors is array of objects
+// ! = variable is required for query to execute
 
 module.exports = typeDefs;
