69dff20cfad7839d5a4be0c463f4292af02c6f55
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-11-29 18:09:35 -0800

    Quick indentation fix to mirror styling from class activities

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,59 +4,59 @@ import { gql } from '@apollo/client';
 // reference API.js & coponents - what does the client need to render correctly once the api request has successfully updated the db as needed
 
 export const LOGIN_USER = gql`
-    mutation login(email: String!, password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
+mutation login(email: String!, password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
         }
     }
+}
 `;
 
 export const CREATE_USER = gql`
-    mutation createUser(username: String!, email: String!, password: String!) {
-        createUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
+mutation createUser(username: String!, email: String!, password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
         }
     }
+}
 `;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook(bookData: BookInput!) {
-        saveBook(bookData: $bookData) {
-            _id
-            username
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
+mutation saveBook(bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+        _id
+        username
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
         }
     }
+}
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook(bookId: String!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
+mutation removeBook(bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
         }
     }
+}
 `;
