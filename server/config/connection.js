172693be2caa34fd6aac9cd10c4636b208210b59
Saraah Hollingsworth <sahhollingsworth@gmail.com>
2021-12-05 16:15:47 -0800

    Updated Mongoose to latest version

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', err => {
+  if(err) throw err;
+  console.log('Connected to MongoDB!')
 });
 
 module.exports = mongoose.connection;
